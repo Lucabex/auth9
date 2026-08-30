a71242bfd7ca7fe53aa7c1b91e43a18accc18648
Luca Bercioux <Lucabex@gmail.com>
2026-08-30 22:21:32 +0100

    Final commit working version

--- Records/Puzzle.cs
+++ Records/Puzzle.cs
@@ -2,6 +2,9 @@ using System.Text.Json.Serialization;
 
 namespace auth9.Records;
 
+public record PuzzleResp(
+    [property:JsonPropertyName("puzzle")] PuzzleDaily Puzzle
+);
 public record PuzzleDaily(
     [property:JsonPropertyName("solution")] List<string> Solution,
     [property:JsonPropertyName("fen")]string Fen
